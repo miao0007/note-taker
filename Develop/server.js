d6814c9583fba0e38eecd0df8cb468ab0495105e
miao0007 <miao188@hotmail.com>
2020-07-31 21:24:14 +0930

    create basic router app in server.js

---
+++ Develop/server.js
@@ -0,0 +1,40 @@
+// Dependencies
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+// set up express app
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// set up app to handle data parsing
+app.use(express.urlencoded({ extended:true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Routes
+
+// route to send user to index page
+app.get("/", function(req,res){
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+// route to send user to note page
+app.get("/notes", function(req,res){
+    res.sendFile(path.json(__dirname, "./public/notes.html"));
+});
+
+// route that send user the db.json file
+app.get("/api/notes", function(req,res){
+    res.sendFile(path.join(__dirname, "./db/db.json"));
+});
+
+
+
+
+
+
+// Starts the server to begin listening
+app.listen(PORT, function(){
+console.log("App listening on PORT: " + PORT);
+});
